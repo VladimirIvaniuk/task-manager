45d5c249fef88f07962bd6a5ad761b27f8840833
Vladimir Ivanyuk <ivanyuk85vladimir@gmail.com>
2019-03-05 15:59:34 +0200

    доделана авторизация

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+foreach ($_POST as $value){
+    if(empty($value)){
+        require 'errors.php';
+        exit();
+    }
+}
+$data=[
+    'email'=>$_POST['email'],
+    'password'=>md5($_POST['password'])
+];
+
+$pdo=new PDO('mysql:host=localhost;dbname=task-manager', 'root', '');
+$sql = "SELECT * FROM users WHERE email=:email AND password=:password";
+$statement=$pdo->prepare($sql);
+$statement->execute($data);
+$user=$statement->fetch();
+if($user){
+    $_SESSION['user']=$user;
+    header('Location:list.php');
+    exit();
+}
+else {
+    $errorMassage = "Ошибка! Неверно логин или пароль";
+    require "login-form.php";
+    exit();
+}
+header('Location:login-form.php');
--- register-form.php
+++ register-form.php
@@ -17,7 +17,7 @@
 
   <body>
     <div class="form-wrapper text-center">
-      <form class="form-signin" action="register.php" method="post" novalidate>
+      <form class="form-signin" action="register.php" method="post">
         <img class="mb-4" src="assets/img/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
         <label for="inputName" class="sr-only">Имя</label>
@@ -27,7 +27,7 @@
         <label for="inputPassword" class="sr-only">Пароль</label>
         <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Пароль" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться</button>
-        <a href="#">Войти</a>
+        <a href="login-form.php">Войти</a>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
       </form>
     </div>
